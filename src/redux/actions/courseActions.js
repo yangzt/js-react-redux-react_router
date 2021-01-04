840f672153a7c7b66402c0036770a0250c47542e
yangzt <zhentao@hotmail.com>
2021-01-04 11:06:03 +0800

    action type constants added

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,4 +1,6 @@
+import * as types from "./actionTypes";
+
 //this function is called a action creator, cause it returns a typical action object
 export function createCourse(course) {
-  return { type: "CREATE_COURSE", course };
+  return { type: types.CREATE_COURSE, course };
 }
